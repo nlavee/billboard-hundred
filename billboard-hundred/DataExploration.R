fa995e67f5b45e492f680a775b27c10b14cbb297
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2015-12-28 16:29:06 +0300

    Added some confidence interval tests for different decades.

--- billboard-hundred/DataExploration.R
+++ billboard-hundred/DataExploration.R
@@ -17,8 +17,24 @@ favstats(topHundred$Rank)
 histogram( ~ WordCount, data = topHundred, v = mean(topHundred$WordCount))
 bwplot(Decade ~ WordCount, data = topHundred)
 densityplot( ~ WordCount, groups = Decade, data = topHundred, 
-             xlim = c(0, 300), auto.key = TRUE)
+             xlim = c(0, 300), auto.key = TRUE,
+             main = "Word Count for Different Decades")
 histogram( ~ Weeks, data = topHundred, v = mean(topHundred$Weeks))
 densityplot( ~ Weeks, groups = Decade, data = topHundred
              , auto.key = TRUE)
 xyplot( WordCount ~ Year, data = topHundred)
+
+## Confindence interval
+sixties <- subset(topHundred, Decade == "1960s", replace = T)
+seventies <- subset(topHundred, Decade == "1970s", replace = T)
+eighties <- subset(topHundred, Decade == "1980s", replace = T)
+nineties <- subset(topHundred, Decade == "1990s", replace = T)
+twothousands <- subset(topHundred, Decade == "2000s", replace = T)
+twotens <- subset(topHundred, Decade == "2010s", replace = T)
+
+t.test(sixties$WordCount, conf.level = 0.95)$conf
+t.test(seventies$WordCount, conf.level = 0.95)$conf
+t.test(eighties$WordCount, conf.level = 0.95)$conf
+t.test(nineties$WordCount, conf.level = 0.95)$conf
+t.test(twothousands$WordCount, conf.level = 0.95)$conf
+t.test(twotens$WordCount, conf.level = 0.95)$conf
