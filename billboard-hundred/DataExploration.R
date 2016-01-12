1ee982d43679e1e58466b872b347eacee684ec62
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2016-01-12 21:13:38 +0300

    Added new graphics for WordCountOverTheYear and also graphics for Top Artists for each decade.

--- billboard-hundred/DataExploration.R
+++ billboard-hundred/DataExploration.R
@@ -31,3 +31,106 @@ weekPerDecade <- dataL %>% group_by(Decade)
 weekPerDecade <- weekPerDecade %>% summarize(avg_week = mean(Weeks))
 
 ggplot(dataL, aes(Weeks, colour = Decade)) + geom_density()
+
+ggplot(dataL, aes(Rank, WordCount, colour = Decade)) + geom_smooth(method="lm", se = FALSE)
+
+top25 <- filter(dataL, Rank <= 25)
+ggplot(top25, aes(Rank, WordCount, colour = Decade)) + geom_smooth(method="lm", se = FALSE)
+
+## Word Count over Time
+topWordCount <- filter(dataL, WordCount >= 450)
+ggplot(dataL, aes(Year, WordCount)) + 
+  geom_point() + 
+  geom_smooth(method="lm", se = FALSE) + 
+  geom_text(data = topWordCount, aes(Year, WordCount, label = paste(Title, "(", as.character(WordCount), ")")), vjust = -1) + 
+  labs( title = "Word Count of Billboard Top 100 songs over the year")
+
+
+ggplot(dataL, aes(Artist)) + geom_bar() + coord_flip() ## too big, not very useful
+
+
+## Top artist for each generation
+
+#1960s
+topArtist1960s <- sixties %>% group_by(Artist) %>%
+  summarize(count_billboard = n()) %>%
+  filter(count_billboard >= 6) %>%
+  arrange(desc(count_billboard))
+
+nrow(topArtist1960s)
+
+ggplot(topArtist1960s, aes(x = factor(Artist), y = count_billboard, label = count_billboard)) + 
+  geom_bar(stat = "identity", fill = "cornflowerblue") + 
+  geom_label(aes(y = count_billboard + 0.5)) + 
+  labs( title = "Top Artist in the 1960s", x = "Artist", y="Count on Billboard") + coord_flip()
+
+#1970s
+topArtist1970s <- seventies %>% group_by(Artist) %>%
+  summarize(count_billboard = n()) %>%
+  filter(count_billboard >= 6) %>%
+  arrange(desc(count_billboard))
+
+nrow(topArtist1970s)
+
+ggplot(topArtist1970s, aes(x = factor(Artist), y = count_billboard, label = count_billboard)) + 
+  geom_bar(stat = "identity", fill = "cornflowerblue") + 
+  geom_label(aes(y = count_billboard + 0.5)) + 
+  labs( title = "Top Artist in the 1970s", x = "Artist", y="Count on Billboard") + coord_flip()
+
+
+#1980s
+topArtist1980s <- eighties %>% group_by(Artist) %>%
+  summarize(count_billboard = n()) %>%
+  filter(count_billboard >= 6) %>%
+  arrange(desc(count_billboard))
+
+nrow(topArtist1980s)
+
+ggplot(topArtist1980s, aes(x = factor(Artist), y = count_billboard, label = count_billboard)) + 
+  geom_bar(stat = "identity", fill = "cornflowerblue") + 
+  geom_label(aes(y = count_billboard + 0.5)) + 
+  labs( title = "Top Artist in the 1980s", x = "Artist", y="Count on Billboard") + coord_flip()
+
+
+#1990s
+topArtist1990s <- nineties %>% group_by(Artist) %>%
+  summarize(count_billboard = n()) %>%
+  filter(count_billboard >= 5) %>%
+  arrange(desc(count_billboard))
+
+nrow(topArtist1990s)
+
+ggplot(topArtist1990s, aes(x = factor(Artist), y = count_billboard, label = count_billboard)) + 
+  geom_bar(stat = "identity", fill = "cornflowerblue") + 
+  geom_label(aes(y = count_billboard + 0.5)) + 
+  labs( title = "Top Artist in the 1990s", x = "Artist", y="Count on Billboard") + coord_flip()
+
+
+#2000s
+topArtist2000s <- twothousands %>% group_by(Artist) %>%
+  summarize(count_billboard = n()) %>%
+  filter(count_billboard >= 7) %>%
+  arrange(desc(count_billboard))
+
+nrow(topArtist2000s)
+
+ggplot(topArtist2000s, aes(x = factor(Artist), y = count_billboard, label = count_billboard)) + 
+  geom_bar(stat = "identity", fill = "cornflowerblue") + 
+  geom_label(aes(y = count_billboard + 0.5)) + 
+  labs( title = "Top Artist in the 2000s", x = "Artist", y="Count on Billboard") + coord_flip()
+
+#2010s
+topArtist2010s <- twotens %>% group_by(Artist) %>%
+            summarize(count_billboard = n()) %>%
+            filter(count_billboard >= 5) %>%
+            arrange(desc(count_billboard))
+
+nrow(topArtist2010s)
+
+ggplot(topArtist2010s, aes(x = factor(Artist), y = count_billboard, label = count_billboard)) + 
+  geom_bar(stat = "identity", fill = "cornflowerblue") + 
+  geom_label(aes(y = count_billboard + 0.5)) + 
+  labs( title = "Top Artist in the 2010s", x = "Artist", y="Count on Billboard") + coord_flip()
+
+
+
