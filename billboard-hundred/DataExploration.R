1bd6ffb445557b84847455900f6b0f402da1648f
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2016-01-12 10:31:44 +0300

    Started data clean up and partitioning data into groups according to decades.

--- billboard-hundred/DataExploration.R
+++ billboard-hundred/DataExploration.R
@@ -1,40 +1,29 @@
-library(mosaic)
+library(mosaic); library(dplyr)
 
-summary(topHundredExtended)
+# shorter name
+dataL <- na.omit(topHundredExtended)
 
-topHundred <- na.omit(topHundredExtended)
-summary(topHundred)
+# get rid of songs without lyrics
+dataL <- filter(dataL, WordCount > 0)
 
-colnames(topHundred)
+# per decade dataset
+sixties <- filter(dataL, Decade == "1960s")
+seventies <- filter(dataL, Decade == "1970s")
+eighties <- filter(dataL, Decade == "1980s")
+nineties <- filter(dataL, Decade == "1990s")
+twothousands <- filter(dataL, Decade == "2000s")
+twotens <- filter(dataL, Decade == "2010s")
 
-## Fastats for all variables
-favstats(topHundred$Year)
-favstats(topHundred$WordCount)
-favstats(topHundred$Weeks)
-favstats(topHundred$Rank)
+# favstats
+favstats(dataL$Weeks)
+favstats(dataL$Rank)
+favstats(dataL$WordCount)
 
-## Simple graphs
-histogram( ~ WordCount, data = topHundred, v = mean(topHundred$WordCount))
-bwplot(Decade ~ WordCount, data = topHundred)
-densityplot( ~ WordCount, groups = Decade, data = topHundred, 
-             xlim = c(0, 300), auto.key = TRUE,
-             main = "Word Count for Different Decades")
-histogram( ~ Weeks, data = topHundred, v = mean(topHundred$Weeks))
-densityplot( ~ Weeks, groups = Decade, data = topHundred
-             , auto.key = TRUE)
-xyplot( WordCount ~ Year, data = topHundred)
+# exploratory graph
+densityplot( ~ WordCount, data = dataL, 
+             groups = Decade, xlim = c(0,350),
+             auto.key = TRUE)
 
-## Confindence interval
-sixties <- subset(topHundred, Decade == "1960s", replace = T)
-seventies <- subset(topHundred, Decade == "1970s", replace = T)
-eighties <- subset(topHundred, Decade == "1980s", replace = T)
-nineties <- subset(topHundred, Decade == "1990s", replace = T)
-twothousands <- subset(topHundred, Decade == "2000s", replace = T)
-twotens <- subset(topHundred, Decade == "2010s", replace = T)
+xyplot(Rank ~ WordCount, data = dataL, groups = Decade)
 
-t.test(sixties$WordCount, conf.level = 0.95)$conf
-t.test(seventies$WordCount, conf.level = 0.95)$conf
-t.test(eighties$WordCount, conf.level = 0.95)$conf
-t.test(nineties$WordCount, conf.level = 0.95)$conf
-t.test(twothousands$WordCount, conf.level = 0.95)$conf
-t.test(twotens$WordCount, conf.level = 0.95)$conf
+bwplot( WordCount ~ Decade, data = dataL)
